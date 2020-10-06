4d87cf804c04f225c4c7132324df7cac7dfe02d5
evlay <elayton32@gmail.com>
2020-10-06 18:40:43 -0500

    added environment variables to prep for addition of postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
 
-const app = express()
+const env = dotenv.config()
 const PORT = process.env.PORT || 3000
+
+// app 
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// Routes
 
 app.get('/', (req, res) => {
     res.send('hello world')
