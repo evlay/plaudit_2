f548aa9ebb6c034205ad50a2bd79ee786f2fdaac
evlay <elayton32@gmail.com>
2020-10-27 21:39:53 -0500

    add express to code and layout routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,50 @@
 import { userInfo } from "os";
 import {createConnection} from "typeorm";
 import {PlauditUser} from "./entity/PlauditUser";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
 
 createConnection(/*...*/).then(async connection => {
 
-    let plauditUser = new PlauditUser();
-    plauditUser.firstName = 'ev'
-    plauditUser.age = 12
-    plauditUser.lastName = 'lay'
+    // let plauditUser = new PlauditUser();
+    // plauditUser.firstName = 'ev'
+    // plauditUser.age = 12
+    // plauditUser.lastName = 'lay'
 
-    await connection.manager.save(plauditUser);
-    console.log("user has been saved with user id " + plauditUser.id);
+    // await connection.manager.save(plauditUser);
+    // console.log("user has been saved with user id " + plauditUser.id);
+
+    app.get('/', (req, res) => {
+        res.send('Home page')
+    })
+
+    app.get('/users', (req, res) => {
+        // Send all users
+    })
+
+    app.get('/user/:id', (req, res) => {
+        // Send specific user by id
+    })
+
+    app.post('/user', (req, res) => {
+        // Create user
+    })
+
+    app.put('/user/:id', (req, res) => {
+        // Update user by id
+    })
+
+    app.delete('/user/:id', (req, res) => {
+        // delete user by id
+    })
+
+    app.listen(PORT, () => {
+        console.log(`express server listening on port ${PORT}`)
+    })
+
 
 }).catch(error => console.log(error));
