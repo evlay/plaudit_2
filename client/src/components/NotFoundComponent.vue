5c3d1d60b96828d77e50c28a950bb99a0056966b
evlay <elayton32@gmail.com>
2020-11-09 20:16:36 -0600

    changed vue to typescript

---
+++ client/src/components/NotFoundComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="NotFoundComponentContainer">
+    <h1>404</h1>
+    <p>Sorry, could not find the page you were looking for</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class NotFoundComponent extends Vue {
+   @Prop() private msg!: string;
+}
+</script>
