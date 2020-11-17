ca0eb3422f6057d2225314abcd030f1cec92046b
evlay <elayton32@gmail.com>
2020-11-16 21:01:54 -0600

    started posts component. Need to cleanup data in mongo and add validation to server for adding new posts

---
+++ client/src/components/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="post-container">
+    <h2>{{ summary }}</h2>
+    <p>{{ body }}</p>
+    <p>Created: {{ createdOn }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Post extends Vue {
+  @Prop() summary!: string
+  @Prop() body!: string
+  @Prop() createdOn!: string
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/views/PostsPage.vue
+++ client/src/views/PostsPage.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="posts-container">
     <h1>Posts</h1>
+    <ul>
+      <Post summary="summary 1" body="body 1" createdOn="1969"/>
+      <button @click="fetchPosts">Fetch</button>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import Post from '@/components/Post'
+import http from '@/utils/http-common'
+
+export default {
+  components: {
+    Post
+  },
+
+  methods: {
+    fetchPosts: () => {
+      http.get('/posts')
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+    }
+  }
+}
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
