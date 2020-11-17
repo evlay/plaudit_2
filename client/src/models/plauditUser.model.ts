b61b45f8c22b7da1b7b7862fafee0f5e88a1f653
evlay <elayton32@gmail.com>
2020-11-16 21:13:03 -0600

    deleted models - used on server... maybe interfaces instead?

--- client/src/models/plauditUser.model.ts
+++
@@ -1,9 +0,0 @@
-export default class PlauditUserModel {
-  username: string;
-  createdOn: string;
-
-  constructor() {
-    this.username = ''
-    this.createdOn = ''
-  }
-}
--- client/src/views/PostsPage.vue
+++ client/src/views/PostsPage.vue
@@ -16,6 +16,11 @@ export default {
   components: {
     Post
   },
+  data() {
+    return {
+      posts: []
+    }
+  },
 
   methods: {
     fetchPosts: () => {
