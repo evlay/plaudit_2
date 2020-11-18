1652460bd055b44c9337e8c64a2bcfb8c16de703
evlay <elayton32@gmail.com>
2020-11-17 22:02:51 -0600

    registered font awesome and added sync with animation on hover to posts page

--- client/src/views/PostsPage.vue
+++ client/src/views/PostsPage.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="posts-container">
-    <h1>Posts</h1>
-    <button @click="fetchPosts">Fetch</button>
-    <ul v-for="post in posts" :key="post.__id">
-      <Post :summary="post.summary" :body="post.body" :createdOn="post.createdOn"/>
-    </ul>
+  <div class="posts-page">
+    <div class="posts-title-container">
+      <h1>Posts</h1>
+      <button>
+        <font-awesome-icon
+          @click="fetchPosts"
+          icon="sync"
+          size="2x"
+          class="fa-spin-hover"
+        />
+      </button>
+    </div>
+    <div class="posts-container">
+      <ul v-for="post in posts" :key="post.__id">
+        <Post
+          :summary="post.summary"
+          :body="post.body"
+          :createdOn="post.createdOn"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -14,27 +29,79 @@ import http from '@/utils/http-common'
 
 export default {
   components: {
-    Post
+    Post,
   },
   data() {
     return {
-      posts: []
+      posts: [],
     }
   },
   methods: {
     fetchPosts() {
-      http.get('/posts')
-        .then(res => {
+      http
+        .get('/posts')
+        .then((res) => {
           this.posts = res.data
           console.log(this.posts)
         })
-        .catch(err => console.log(err))
-    }
+        .catch((err) => console.log(err))
+    },
   },
-  mounted(){
+  mounted() {
     this.fetchPosts()
-  }
+  },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import '../styles/colors';
+
+.posts-page {
+  button {
+    background: transparent;
+    border: none;
+    outline: none;
+    // background: coral;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  .posts-title-container {
+    // display: inline-flex;
+    // align-items: center;
+    min-height: 56px;
+    width: 100%;
+  }
+
+  .fa-spin-hover {
+    color: $slate;
+  }
+
+  .fa-spin-hover:hover {
+    animation: fa-spin 2s infinite linear;
+  }
+  // The animation bellow is taken from font-awesome.css
+  @-webkit-keyframes fa-spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
+    }
+  }
+  @keyframes fa-spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(359deg);
+      transform: rotate(359deg);
+    }
+  }
+}
+</style>
